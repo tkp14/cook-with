7f964bca09398f59412da10e5b6279388ac9c42c
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-09-30 15:33:11 +0900

    Rspec gem追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_pages#home'
-  get :about,        to: 'static_pages#about'
-  get :use_of_terms, to: 'static_pages#terms'
+ root 'static_pages#home'
+ get :about,        to: 'static_pages#about'
+ get :use_of_terms, to: 'static_pages#terms'
 end
