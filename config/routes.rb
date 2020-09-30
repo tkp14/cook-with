478ef4a427b6fb265e6716d31a82b55ca53989b6
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-09-30 16:27:58 +0900

    bin/rspecが実行できるまで

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/about'
+  get 'static_pages/terms'
  root 'static_pages#home'
  get :about,        to: 'static_pages#about'
  get :use_of_terms, to: 'static_pages#terms'
