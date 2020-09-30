4bcb17d05476f455ac7d46b59d85c2cf21640895
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-09-30 14:52:13 +0900

    StaticPagesコントローラー作成

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,6 @@ module CookWith
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
-
     config.generators do |g|
       g.assets false # CSS, JavaScriptファイルは生成しない
       g.test_framework false # testファイルは生成しない
