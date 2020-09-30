0d02dd44692165512a5b8c7ee90de0d67d85a374
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-09-30 14:34:26 +0900

    rails generateの自動生成ファイルを設定

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,18 @@ module CookWith
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.2
+
+    config.generators do |g|
+      g.assets false # CSS, JavaScriptファイルは生成しない
+      g.test_framework false # testファイルは生成しない
+      g.test_framework :rspec,
+                       controller_specs: false,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
