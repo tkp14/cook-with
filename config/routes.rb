4f3fb16dbcdea2c1d041c1ca914a046cbf95dd2c
tkp14 <tk.091438@gmail.com>
2020-10-31 02:42:38 +0000

    ユーザーの基本機能 ユーザーコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/show'
+  get 'users/index'
   root 'static_pages#home'
   get :about,        to: 'static_pages#about'
   get :use_of_terms, to: 'static_pages#terms'
