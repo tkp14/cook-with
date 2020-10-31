f3c12c484c5515ec032753948b0982bfcd88177f
tkp14 <tk.091438@gmail.com>
2020-10-31 06:20:31 +0000

    ユーザーの基本機能 モデルの作成

---
+++ db/migrate/20201031025713_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email
+      t.text :introduction
+      t.string :sex
+
+      t.timestamps
+    end
+  end
+end
