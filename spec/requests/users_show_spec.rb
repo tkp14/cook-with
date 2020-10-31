fa8fffec218ede7e830d0e15fa9d6834a8690719
tkp14 <tk.091438@gmail.com>
2020-10-31 18:09:45 +0000

    ユーザー登録処理、プロフィールページに関するシステムテスト修正2

--- spec/requests/users_show_spec.rb
+++ spec/requests/users_show_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 RSpec.describe "プロフィールページ", type: :request do
-  let!(user) { create(:user) }
+  let!(:user) { create(:user) }
 
   it "レスポンスが正常に表示されること" do
     get user_path(user)
