493c957964c353a3480b91826fdbca12eb702825
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-11-02 02:50:49 +0900

    ログイン/ログアウトのシステムテストを追加,登録と同時にログインできる様にする

--- spec/system/sessions_spec.rb
+++ spec/system/sessions_spec.rb
@@ -31,5 +31,34 @@ RSpec.describe "Sessions", type: :system do
         expect(page).to have_button 'ログイン'
       end
     end
+
+    context "ログイン処理" do
+      it "無効なユーザーでログインを行うとログインが失敗することを確認" do
+        fill_in "user_email", with: "user@example.com"
+        fill_in "user_password", with: "pass"
+        click_button "ログイン"
+        expect(page).to have_content 'メールアドレスとパスワードの組み合わせが誤っています'
+
+        visit root_path
+        expect(page).not_to have_content "メールアドレスとパスワードの組み合わせが誤っています"
+      end
+
+      it "有効なユーザーでログインする前後でヘッダーが正しく表示されていることを確認" do
+        expect(page).to have_link 'クック-ウィズとは？', href: about_path
+        expect(page).to have_link 'ユーザー登録(無料)', href: signup_path
+        expect(page).to have_link 'ログイン', href: login_path
+        expect(page).not_to have_link 'ログアウト', href: logout_path
+
+        fill_in "user_email", with: user.email
+        fill_in "user_password", with: user.password
+        click_button "ログイン"
+
+        expect(page).to have_link 'クック-ウィズとは？', href: about_path
+        expect(page).to have_link 'ユーザー一覧', href: users_path
+        expect(page).to have_link 'プロフィール', href: user_path(user)
+        expect(page).to have_link 'ログアウト', href: logout_path
+        expect(page).not_to have_link 'ログイン', href: login_path
+      end
+    end
   end
 end
