b25c629aeda39d97c1962661966fc8e3b943c5e9
tkp14 <tk.091438@gmail.com>
2020-11-01 06:10:55 +0000

    ユーザー登録処理、プロフィールページに関するシステムテスト 修正5

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "クックウィズへようこそ！"
+      flash[:success] = "クック-ウィズへようこそ！"
       redirect_to @user
     else
       render 'new'
