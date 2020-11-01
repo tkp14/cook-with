6c2dfaf5715688f92fcfde0f98c2a93ee185af47
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-11-01 18:17:06 +0900

    ログインページ コントローラー,ルーティング作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'static_pages#home'
   get :about,        to: 'static_pages#about'
   get :use_of_terms, to: 'static_pages#terms'
   get :signup,       to: 'users#new'
+  get    :login,     to: 'sessions#new'
+  post   :login,     to: 'sessions#create'
+  delete :logout,    to: 'sessions#destroy'
   resources :users
 end
