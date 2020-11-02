b75c4fa6d49a6ff9963382eb23b0d24b484b16b9
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-11-03 00:24:40 +0900

    ユーザーのログイン情報が保持される実装

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -17,4 +17,25 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
-end
+
+  # ユーザーのセッションを永続的にする
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  # 記憶トークンcookieに対応するユーザーを返す
+  def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
+    end
+  end
