62ebb4f43a8f73e49b8da8146ee3efaebcf63452
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-11-03 00:47:11 +0900

    永続的セッションの破棄の処理を作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,6 +35,11 @@ class User < ApplicationRecord
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
+  # ユーザーのログイン情報を破棄する
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+
   private
     def downcase_email
       self.email = email.downcase
