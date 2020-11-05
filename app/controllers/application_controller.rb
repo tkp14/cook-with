9dbf104675ad500dd2f3098df4e92cc5ffd5df19
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-11-05 23:09:20 +0900

    フレンドリーフォワーディングの実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
     # ログイン済みユーザーかどうか確認
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] = "ログインしてください"
         redirect_to login_url
       end
