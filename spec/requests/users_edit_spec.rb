9221ad2990b08fd2587e43f7eed974658723357c
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-11-05 22:16:51 +0900

    プロフィール編集のテスト,ログイン処理を追記

--- spec/requests/users_edit_spec.rb
+++ spec/requests/users_edit_spec.rb
@@ -2,27 +2,54 @@ require "rails_helper"
 
 RSpec.describe "プロフィール編集", type: :request do
   let!(:user) { create(:user) }
+  let!(:other_user) { create(:user) }
 
-  before do
-    get edit_user_path(user)
+  context "認可されたユーザーの場合" do
+    it "レスポンスが正常に表示されること" do
+      login_for_request(user)
+      get edit_user_path(user)
+      expect(response).to render_template('users/edit')
+      patch user_path(user), params: { user: { name: "Example User",
+                                               email: "user@example.com",
+                                               introduction: "初めまして",
+                                               sex: "男性" } }
+      redirect_to user
+      follow_redirect!
+      expect(response).to render_template('users/show')
+    end
   end
 
-  it "有効なプロフィール編集" do
-    expect(response).to render_template('users/edit')
-    patch user_path(user), params: { user: { name: "Example User",
-                                             email: "user@example.com",
-                                             introduction: "初めまして",
-                                             sex: "男性" } }
-    redirect_to user
-    follow_redirect!
-    expect(response).to render_template('users/show')
+  context "ログインしていないユーザーの場合" do
+    it "ログイン画面にリダイレクトすること" do
+      get edit_user_path(user)
+      # 編集
+      get edit_user_path(user)
+      expect(response).to have_http_status "302"
+      expect(response).to redirect_to login_path
+      # 更新
+      patch user_path(user), params: { user: { name: "Example User",
+                                               email: "user@example.com",
+                                               introduction: "初めまして",
+                                               sex: "男性" } }
+      expect(response).to have_http_status "302"
+      expect(response).to redirect_to login_path
+    end
   end
 
-  it "無効なプロフィール編集" do
-    expect(response).to render_template('users/edit')
-    patch user_path(user), params: { user: { name: "",
-                                             introduction: "初めまして",
-                                             sex: "男性" } }
-    expect(response).to render_template('users/edit')
+  context "別アカウントのユーザーの場合" do
+    it "ホーム画面にリダイレクトすること" do
+      # 編集
+      login_for_request(other_user)
+      get edit_user_path(user)
+      expect(response).to have_http_status "302"
+      expect(response).to redirect_to root_path
+      # 更新
+      patch user_path(user), params: { user: { name: "Example User",
+                                               email: "user@example.com",
+                                               introduction: "初めまして",
+                                               sex: "男性" } }
+      expect(response).to have_http_status "302"
+      expect(response).to redirect_to root_path
+    end
   end
 end
--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -41,6 +41,7 @@ RSpec.describe "Users", type: :system do
 
   describe "プロフィール編集ページ" do
     before do
+      login_for_system(user)
       visit user_path(user)
       click_link "プロフィール編集"
     end
@@ -80,6 +81,7 @@ RSpec.describe "Users", type: :system do
   describe "プロフィールページ" do
     context "ページレイアウト" do
       before do
+        login_for_system(user)
         visit user_path(user)
       end
 
