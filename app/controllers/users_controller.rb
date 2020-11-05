aadb68dedb6eda8931b6589caeafbe168b7c58bb
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-11-05 18:02:51 +0900

    before_actionを定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :show, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
   def index
   end
 
@@ -46,5 +48,14 @@ class UsersController < ApplicationController
   # プロフィール編集時に許可する属性
   def user_params_update
     params.require(:user).permit(:name, :email, :introduction, :sex)
+  end
+
+  # 正しいユーザーかどうか確認
+  def correct_user
+    @user = User.find(params[:id])
+    if !current_user?(@user)
+      flash[:danger] = "このページへはアクセスできません"
+      redirect_to(root_url)
+    end
   end
 end
