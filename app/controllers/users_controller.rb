f17165f8799f07fbfc5eb6c243ed04757a359101
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-11-06 01:22:06 +0900

    ユーザーの一覧の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :show, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
   def index
+    @users = User.all
   end
 
   def show
