3578bb52d9fcee01324a5086e9a404e8339b2497
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-11-06 17:40:53 +0900

    Userファクトリにadmin属性追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,5 +6,9 @@ FactoryBot.define do
     password_confirmation { "foobar" }
     introduction { "はじめまして。料理初心者ですが、頑張ります！" }
     sex { "男性" }
+
+    trait :admin do
+      admin { true }
+    end
   end
 end
