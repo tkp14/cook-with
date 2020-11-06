48fad294e3769c4a8cec08012aa28b1263ae2106
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-11-06 16:14:22 +0900

    サンプルユーザー作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,14 @@
-# frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(name:  "吉岡 里帆",
+            email: "sample@example.com",
+            password:              "foobar",
+            password_confirmation: "foobar")
+
+99.times do |n|
+ name  = Faker::Name.name
+ email = "sample-#{n+1}@example.com"
+ password = "password"
+ User.create!(name:  name,
+              email: email,
+              password:              password,
+              password_confirmation: password)
+end
