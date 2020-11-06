f30ec85619779d471cd5ef781eb854c19d2d2c36
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-11-06 16:49:23 +0900

    コントローラー,ビューを追記

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :show, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page])
   end
 
   def show
