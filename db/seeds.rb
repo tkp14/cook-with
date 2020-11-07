648c146378507063ba6803e68ce87d7896230e37
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-11-08 01:11:30 +0900

    プロフィールページに料理一覧表示部を追記

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,15 @@ User.create!(name:  "吉岡 里帆",
               password:              password,
               password_confirmation: password)
 end
+
+10.times do |n|
+  Dish.create!(name: Faker::Food.dish,
+               description: "冬に食べたくなる、身体が温まる料理です",
+               portion: 1.5,
+               tips: "ピリッと辛めに味付けするのがオススメ",
+               reference: "https://cookpad.com/recipe/2798655",
+               required_time: 30,
+               popularity: 5,
+               cook_memo: "初めて作った割にはうまくできた！",
+               user_id: 1)
+end
