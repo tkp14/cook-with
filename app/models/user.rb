a9b9ac601ef95c38b57a916b0de55354dc7e06b5
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-11-07 23:17:44 +0900

    ユーザーが消されると紐づく料理も消されるようにする

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :dishes, dependent: :destroy
   attr_accessor :remember_token
   before_save :downcase_email
   validates :name, presence: true, length: { maximum: 50 }
