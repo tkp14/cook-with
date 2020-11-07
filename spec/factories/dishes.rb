e4d6695ae9daaa28a209cda26e42b13fdd9481a1
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-11-07 17:43:04 +0900

    テスト用のファクトリの修正

--- spec/factories/dishes.rb
+++ spec/factories/dishes.rb
@@ -1,13 +1,12 @@
 FactoryBot.define do
   factory :dish do
-    name "MyString"
-    description "MyText"
-    portion 1.5
-    tips "MyText"
-    reference "MyText"
-    required_time 1
-    popularity 1
-    cook_memo "MyText"
-    user nil
+    name { Faker::Food.dish }
+    description { "冬に食べたくなる、身体が温まる料理です" }
+    portion { 1.5 }
+    tips { "ピリッと辛めに味付けするのがオススメ" }
+    reference { "https://cookpad.com/recipe/2798655" }
+    required_time { 30 }
+    popularity { 5 }
+    association :user
   end
 end
