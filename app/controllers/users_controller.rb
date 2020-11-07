b37d767ee0fecf0a5cdcaf32ea8cf744a5aa08d6
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-11-07 23:33:56 +0900

    Dishesコントローラ作成,料理一覧を表示する

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @dishes = @user.dishes.paginate(page: params[:page], per_page: 5)
   end
 
   def new
