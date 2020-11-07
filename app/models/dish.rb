f110969983cc7334eebf386eb9ac3440a67131f3
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-11-07 17:41:15 +0900

    バリデーションの追加

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,3 +1,14 @@
 class Dish < ApplicationRecord
   belongs_to :user
+  validates :user_id, presence: true
+  validates :name, presence: true, length: { maximum: 30 }
+  validates :description, length: { maximum: 140 }
+  validates :tips, length: { maximum: 50 }
+  validates :popularity,
+            :numericality => {
+              :only_interger => true,
+              :greater_than_or_equal_to => 1,
+              :less_than_or_equal_to => 5
+            },
+            allow_nil: true
 end
