875d06e4e8fac2787b07275edc77868065517d31
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-11-07 22:54:33 +0900

    投稿した料理が新しい順に表示されるようにする

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,5 +1,6 @@
 class Dish < ApplicationRecord
   belongs_to :user
+  default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :name, presence: true, length: { maximum: 30 }
   validates :description, length: { maximum: 140 }
--- spec/factories/dishes.rb
+++ spec/factories/dishes.rb
@@ -8,5 +8,18 @@ FactoryBot.define do
     required_time { 30 }
     popularity { 5 }
     association :user
+    created_at { Time.current }
+  end
+
+  trait :yesterday do
+    created_at { 1.day.ago }
+  end
+
+  trait :one_week_ago do
+    created_at { 1.week.ago }
+  end
+
+  trait :one_month_ago do
+    created_at { 1.month.ago }
   end
 end
