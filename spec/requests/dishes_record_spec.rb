72ec38ad1bed7b055808c904f89bee41a27db516
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-11-10 15:32:58 +0900

    料理登録のテスト記述

--- spec/requests/dishes_record_spec.rb
+++ spec/requests/dishes_record_spec.rb
@@ -5,10 +5,40 @@ RSpec.describe "お料理登録", type: :request do
   let!(:dish) { create(:dish, user: user) }
 
   context "ログインしているユーザーの場合" do
-    it "レスポンスが正常に表示されること" do
-      login_for_requests(user)
+    before do
+      login_for_request(user)
       get new_dish_path
+    end
+
+    it "レスポンスが正常に表示されること" do
       expect(response).to have_http_status "200"
+      expect(response).to render_template('dishes/new')
+    end
+
+    it "有効な料理データで登録できること" do
+      expect {
+        post dishes_path, params: { dish: { name: "イカの塩焼き",
+                                            description: "冬に食べたくなる、身体が温まる料理です",
+                                            portion: 1.5,
+                                            tips: "ピリッと辛めに味付けするのがオススメ",
+                                            reference: "https://cookpad.com/recipe/2798655",
+                                            required_time: 30,
+                                            popularity: 5 } }
+      }.to change(Dish, :count).by(1)
+      follow_redirect!
+      expect(response).to render_template('static_pages/home')
+    end
+
+    it "無効な料理データでは登録できないこと" do
+      expect {
+        post dishes_path, params: { dish: { name: "",
+                                            description: "冬に食べたくなる、身体が温まる料理です",
+                                            portion: 1.5,
+                                            tips: "ピリッと辛めに味付けするのがオススメ",
+                                            reference: "https://cookpad.com/recipe/2798655",
+                                            required_time: 30,
+                                            popularity: 5 } }
+      }.not_to change(Dish, :count)
       expect(response).to render_template('dishes/new')
     end
   end
---
+++ spec/system/dishes_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+
+RSpec.describe "Dishes", type: :system do
+  let!(:user) { create(:user) }
+
+  describe "料理の登録ページ" do
+    before do
+      login_for_system(user)
+      visit new_dish_path
+    end
+
+    context "ページレイアウト" do
+      it "「料理の登録」の文字列が存在すること" do
+        expect(page).to have_content '料理の登録'
+      end
+
+      it "正しいタイトルが表示されること" do
+        expect(page).to have_title full_title('料理の登録')
+      end
+
+      it "入力部分に適切なラベルが表示されること" do
+        expect(page).to have_content '料理名'
+        expect(page).to have_content '説明'
+        expect(page).to have_content '分量 [人分]'
+        expect(page).to have_content 'コツ・ポイント'
+        expect(page).to have_content '作り方参照用URL'
+        expect(page).to have_content '所要時間 [分]'
+        expect(page).to have_content '人気度 [1~5]'
+        expect(page).to have_content 'クックメモ'
+      end
+    end
+
+    context "ユーザー登録処理" do
+      it "有効な情報で料理登録を行うと料理登録成功のフラッシュが表示されること" do
+        fill_in "料理名", with: "イカの塩焼き"
+        fill_in "説明", with: "冬に食べたくなる、身体が温まる料理です"
+        fill_in "分量", with: 1.5
+        fill_in "コツ・ポイント", with: "ピリッと辛めに味付けするのがオススメ"
+        fill_in "作り方参照用URL", with: "https://cookpad.com/recipe/2798655"
+        fill_in "所要時間", with: 30
+        fill_in "人気度", with: 5
+        click_button "登録する"
+        expect(page).to have_content "料理が登録されました！"
+      end
+
+      it "無効な情報で料理登録を行うと料理登録失敗のフラッシュが表示されること" do
+        fill_in "料理名", with: ""
+        fill_in "説明", with: "冬に食べたくなる、身体が温まる料理です"
+        fill_in "分量", with: 1.5
+        fill_in "コツ・ポイント", with: "ピリッと辛めに味付けするのがオススメ"
+        fill_in "作り方参照用URL", with: "https://cookpad.com/recipe/2798655"
+        fill_in "所要時間", with: 30
+        fill_in "人気度", with: 5
+        click_button "登録する"
+        expect(page).to have_content "料理名を入力してください"
+      end
+    end
+  end
+end
