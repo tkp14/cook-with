f9178fb114a15d9d00a7154e0a3d454e269f36e6
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-11-11 02:15:07 +0900

    料理の削除処理の実装

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -34,6 +34,18 @@ class DishesController < ApplicationController
     end
   end
 
+  def destroy
+    @dish = Dish.find(params[:id])
+    if current_user.admin? || current_user?(@dish.user)
+      @dish.destroy
+      flash[:success] = "料理が削除されました"
+      redirect_to request.referrer == user_url(@dish.user) ? user_url(@dish.user) : root_url
+    else
+      flash[:danger] = "他人の料理は削除できません"
+      redirect_to root_url
+    end
+  end
+
   private
 
   def dish_params
