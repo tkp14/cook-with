ad3be74859f2781f653ae531139b0974703467fd
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-11-10 14:33:05 +0900

    料理の登録処理作成

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -4,4 +4,21 @@ class DishesController < ApplicationController
   def new
     @dish = Dish.new
   end
+
+  def create
+    @dish = Dish.find(params[:id])
+    if @dish.save
+      flash[:success] = "料理が登録されました！"
+      redirect_to root_url
+    else
+      render "dishes/new"
+    end
+  end
+
+  private
+
+  def dish_params
+    params.require(:dish).permit(:name, :discription, :portion, :tips,
+                                 :reference, :required_time, :popularity, :cook_memo)
+  end
 end
