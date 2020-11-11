5d4e349e7d285dc8549e2a48a6d611337aad718d
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-11-11 21:29:12 +0900

    dishモデルにmount_uploader追加

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,6 +1,7 @@
 class Dish < ApplicationRecord
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
+  mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
   validates :name, presence: true, length: { maximum: 30 }
   validates :description, length: { maximum: 140 }
