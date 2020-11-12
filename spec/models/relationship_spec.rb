2678c694c97277cfac52ef9798a4caa7d977b4cd
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-11-12 23:52:22 +0900

    定義したメソッドが機能していることを確認するテストを追加

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Relationship, type: :model do
   let!(:relationship) { create(:relationship) }
+  let(:user) { create(:user) }
+  let!(:other_user) { create(:user) }
 
   it "関係性が有効であること" do
     expect(relationship).to be_valid
@@ -15,5 +17,15 @@ RSpec.describe Relationship, type: :model do
   it "followed_idがnilの場合、関係性が無効であること" do
     relationship.followed_id = nil
     expect(relationship).not_to be_valid
+  end
+
+  context "フォロー機能" do
+    it "フォローとアンフォローが正常に動作すること" do
+      expect(user.following?(other_user)).to be_falsey
+      user.follow(other_user)
+      expect(user.following?(other_user)).to be_truthy
+      user.unfollow(other_user)
+      expect(user.following?(other_user)).to be_falsey
+    end
   end
 end
