552e340aeecfc90a65415a067bb5f60726f3a7b5
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-11-12 21:41:20 +0900

    マイグレーションファイル修正

--- db/migrate/20201112123217_create_relationships.rb
+++ db/migrate/20201112123217_create_relationships.rb
@@ -3,11 +3,10 @@ class CreateRelationships < ActiveRecord::Migration[5.2]
     create_table :relationships do |t|
       t.integer :follower_id
       t.integer :followed_id
-
       t.timestamps
-      add_index :relationships, :follower_id
-      add_index :relationships, :followed_id
-      add_index :relationships, [:follower_id, :followed_id], unique: true
     end
+    add_index :relationships, :follower_id
+    add_index :relationships, :followed_id
+    add_index :relationships, [:follower_id, :followed_id], unique: true
   end
 end
