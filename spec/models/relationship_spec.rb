52bc9fd21c7a0b977564c4dfa1f643211d477ae1
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-11-12 23:23:13 +0900

    Relationshipモデルのバリデーションのテスト

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Relationship, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:relationship) { create(:relationship) }
+
+  it "関係性が有効であること" do
+    expect(relationship).to be_valid
+  end
+
+  it "follower_idがnilの場合、関係性が無効であること" do
+    relationship.follower_id = nil
+    expect(relationship).not_to be_valid
+  end
+
+  it "followed_idがnilの場合、関係性が無効であること" do
+    relationship.followed_id = nil
+    expect(relationship).not_to be_valid
+  end
 end
