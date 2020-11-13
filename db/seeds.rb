a4dd4dee6a2bc42d946bb595bf95d7e8392408e3
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-11-13 09:38:01 +0900

    フォローのサンプルデータを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,3 +25,11 @@ end
                cook_memo: "初めて作った割にはうまくできた！",
                user_id: 1)
 end
+
+# リレーションシップ
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
