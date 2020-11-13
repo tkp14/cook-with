33a0ad719b72ed2b233cacea4204a593cdb586ad
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-11-13 10:58:29 +0900

    プロフィールページにフォローボタン作成

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,8 +1,9 @@
 module SessionsHelper
+  # 渡されたユーザーでログインする
   def log_in(user)
     session[:user_id] = user.id
   end
-
+  # ユーザーがログインしていればtrue、その他ならfalseを返す
   def logged_in?
     !current_user.nil?
   end
