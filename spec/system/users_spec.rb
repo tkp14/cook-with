9b01f6fd5af72611a5e35e49126609ad17b28516
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-11-14 00:34:09 +0900

    テスト作成

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :system do
   let!(:user) { create(:user) }
+  let!(:other_user) { create(:user) }
   let!(:admin_user) { create(:user, :admin) }
 
   describe "ユーザー一覧ページ" do
@@ -162,6 +163,18 @@ RSpec.describe "Users", type: :system do
 
       it "料理のページネーションが表示されていることを確認" do
         expect(page).to have_css "div.pagination"
+      end
+    end
+
+    context "ユーザーのフォロー/アンフォロー処理", js: true do
+      it "ユーザーのフォロー/アンフォローができること" do
+        login_for_system(user)
+        visit user_path(other_user)
+        expect(page).to have_button 'フォローする'
+        click_button 'フォローする'
+        expect(page).to have_button 'フォロー中'
+        click_button 'フォロー中'
+        expect(page).to have_button 'フォローする'
       end
     end
   end
