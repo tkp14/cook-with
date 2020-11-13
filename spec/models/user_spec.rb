652577edf8a61365f264ad8eeb052f412cfbb14b
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-11-13 09:30:00 +0900

    Userモデルにフォローされている場合のメソッドを追記,修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   let(:user) { create(:user) }
+  let!(:other_user) { create(:user) }
 
   context "バリデーション" do
     it "名前、メールアドレスがあれば有効な状態であること" do
@@ -62,4 +63,14 @@ RSpec.describe User, type: :model do
       expect(user.authenticated?('')).to eq false
     end
   end
-end
+
+  context "フォロー機能" do
+    it "フォローとアンフォローが正常に動作すること" do
+      expect(user.following?(other_user)).to be_falsey
+      user.follow(other_user)
+      expect(user.following?(other_user)).to be_truthy
+      expect(other_user.followed_by?(user)).to be_truthy
+      user.unfollow(other_user)
+      expect(user.following?(other_user)).to be_falsey
+    end
+  end
