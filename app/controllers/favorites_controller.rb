348dc4df625bf41d03d8b170ae388edd159fb30c
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-11-16 01:15:32 +0900

    Ajaxを可能にするため,Favoritesコントローラにcreate, destroyアクションを実装する

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,8 +2,21 @@ class FavoritesController < ApplicationController
   before_action :logged_in_user
 
   def create
+    @dish = Dish.find(params[:dish_id])
+    @user = @dish.user
+    current_user.favorite(@dish)
+    respond_to do |format|
+      format.html { redirect_to request.referrer || root_url }
+      format.js
+    end
   end
 
   def destroy
+    @dish = Dish.find(params[:dish_id])
+    current_user.favorites.find_by(dish_id: @dish.id).destroy
+    respond_to do |format|
+      format.html { redirect_to request.referrer || root_url }
+      format.js
+    end
   end
 end
