1ef14113035ab980d9202d75ca38b71b19a75035
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-11-16 02:33:45 +0900

    テスト追加

--- spec/requests/favorites_spec.rb
+++ spec/requests/favorites_spec.rb
@@ -5,6 +5,38 @@ RSpec.describe "お気に入り登録機能", type: :request do
   let!(:other_user) { create(:user) }
 
   context "お気に入り登録処理" do
+    context "ログインしている場合" do
+      before do
+        login_for_requests(user)
+      end
+
+      it "料理のお気に入り登録ができること" do
+        expect {
+          post "/favorites/#{dish.id}/create"
+        }.to change(user.favorites, :count).by(1)
+      end
+
+      it "料理のAjaxによるお気に入りができること" do
+        expect {
+          post "/favorites/#{dish.id}/create",xhr: true
+        }.to change(user.favorites, :count).by(1)
+      end
+
+      it "料理のお気に入り解除できること" do
+        user.favorite(dish)
+        expect {
+          delete "/favorites/#{dish.id}/destroy"
+        }.to change(user.favorites, :count).by(-1)
+      end
+
+      it "料理のAjaxによるお気に入り解除ができること" do
+        user.favorite(dish)
+        expect {
+          delete "/favorites/#{dish.id}/destroy",xhr: true
+        }.to change(user.favorites, :count).by(-1)
+      end
+    end
+
     context "ログインしていない場合" do
       it "お気に入り登録は実行できず、ログインページへリダイレクトすること" do
         expect {
--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -191,6 +191,42 @@ RSpec.describe "Users", type: :system do
         user.unfavorite(dish)
         expect(user.favorite?(dish)).to be_falsey
       end
+
+     it "トップページからお気に入り登録/解除ができること", js: true do
+        visit root_path
+        link = find('.like')
+        expect(link[:href]).to include "/favorites/#{dish.id}/create"
+        link.click
+        link = find('.unlike')
+        expect(link[:href]).to include "/favorites/#{dish.id}/destroy"
+        link.click
+        link = find('.like')
+        expect(link[:href]).to include "/favorites/#{dish.id}/create"
+      end
+
+      it "ユーザー個別ページからお気に入り登録/解除ができること", js: true do
+        visit user_path(user)
+        link = find('.like')
+        expect(link[:href]).to include "/favorites/#{dish.id}/create"
+        link.click
+        link = find('.unlike')
+        expect(link[:href]).to include "/favorites/#{dish.id}/destroy"
+        link.click
+        link = find('.like')
+        expect(link[:href]).to include "/favorites/#{dish.id}/create"
+      end
+
+      it "料理個別ページからお気に入り登録/解除ができること", js: true do
+        visit dish_path(dish)
+        link = find('.like')
+        expect(link[:href]).to include "/favorites/#{dish.id}/create"
+        link.click
+        link = find('.unlike')
+        expect(link[:href]).to include "/favorites/#{dish.id}/destroy"
+        link.click
+        link = find('.like')
+        expect(link[:href]).to include "/favorites/#{dish.id}/create"
+      end
     end
   end
 end
