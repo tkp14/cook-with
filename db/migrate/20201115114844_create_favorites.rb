b144166f9bcf96b8e1bc0a26c03c845e6b08f9a2
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-11-15 22:42:18 +0900

    Favoriteモデルの作成,マイグレーションファイルに追記

---
+++ db/migrate/20201115114844_create_favorites.rb
@@ -0,0 +1,11 @@
+class CreateFavorites < ActiveRecord::Migration[5.2]
+  def change
+    create_table :favorites do |t|
+      t.integer :user_id
+      t.integer :dish_id
+
+      t.timestamps
+    end
+    add_index :favorites, [:user_id, :dish_id], unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_12_123217) do
+ActiveRecord::Schema.define(version: 2020_11_15_114844) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,14 @@ ActiveRecord::Schema.define(version: 2020_11_12_123217) do
     t.string "picture"
     t.index ["user_id", "created_at"], name: "index_dishes_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_dishes_on_user_id"
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "dish_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "dish_id"], name: "index_favorites_on_user_id_and_dish_id", unique: true
   end
 
   create_table "relationships", force: :cascade do |t|
