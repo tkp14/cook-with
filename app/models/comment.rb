c566616ac5e10728e75c58494b49c09d9cbe5ba7
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-11-16 21:02:20 +0900

    関連付けとバリデーションを追加

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ApplicationRecord
-  
+  belongs_to :dish
+  validates :user_id, presence: true
+  validates :dish_id, presence: true
+  validates :content, presence: true, length: { maximum: 50 }
 end
--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,6 +1,7 @@
 class Dish < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
+  has_many :comments, dependent: :destroy
   default_scope -> { order(created_at: :desc) }
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
