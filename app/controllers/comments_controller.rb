c4074b8afd126c75815d64a24268501e21b18cfd
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-11-17 01:03:13 +0900

    コメントの削除機能の実装,修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   before_action :logged_in_user
 
   def create
-    @dish = Dish.find(params[:id])
+    @dish = Dish.find(params[:dish_id])
     @user = @dish.user
     @comment = @dish.comments.build(user_id: current_user.id, content: params[:comment][:content])
     if !@dish.nil? && @comment.save
@@ -13,7 +13,7 @@ class CommentsController < ApplicationController
     redirect_to request.referrer || root_url
   end
 
-  def delete
+  def destroy
     @comment = Comment.find(params[:id])
     @dish = @comment.dish
     if current_user.id == @comment.user_id
