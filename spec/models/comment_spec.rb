f131667bbbbb73c14bfb7c44b473a071f02d88ad
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-11-16 21:30:31 +0900

    テスト記述

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:comment) { create(:comment) }
+
+  context "バリデーション" do
+    it "有効であること" do
+      expect(comment).to be_valid
+    end
+
+    it "user_idがnilの場合無効であること" do
+      comment = build(:comment, user_id: nil)
+      expect(comment).not_to be_valid
+    end
+
+    it "dish_idがnilの場合無効であること" do
+      comment = build(:comment, dish_id: nil)
+      expect(comment).not_to be_valid
+    end
+
+    it "内容がnilの場合無効であること" do
+      comment = build(:comment, context: nil)
+      expect(comment).not_to be_valid
+    end
+
+    it "内容が50文字以内であること" do
+      comment = build(:comment, context: "あ" * 51)
+      comment.valid?
+      expect(comment.errors[:content]).to include("は50文字以内で入力してください")
+    end
+  end
 end
