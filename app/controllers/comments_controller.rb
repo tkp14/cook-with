ce426fa69254b8e47800a04df2db1c3b86003559
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-11-16 23:50:16 +0900

    コメントを入力できるようにする

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,16 @@
 class CommentsController < ApplicationController
   before_action :logged_in_user
-  
+
   def create
+    @dish = Dish.find(params[:id])
+    @user = @dish.user
+    @comment = @dish.comments.build(user_id: current_user.id, content: params[:comment][:content])
+    if !@dish.nil? && @comment.save
+      flash[:success] = "コメントを追加しました！"
+    else
+      flash[:danger] = "空のコメントは投稿できません。"
+    end
+    redirect_to request.referrer || root_url
   end
 
   def delete
