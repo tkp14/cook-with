4469d22b1f9060ceb160c5692a3dc5190cd45cf4
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-11-17 14:50:17 +0900

    テスト記述

--- spec/models/notification_spec.rb
+++ spec/models/notification_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Notification, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:notification) { create(:notification) }
+
+  it "通知機能がが有効であること" do
+    expect(notification).to be_vaid
+  end
+
+  it "user_idがnilの場合、無効であること" do
+    notification.user_id = nil
+    expect(notification).not_to be_valid
+  end
+
+  it "dish_idがnilの場合、無効であること" do
+    notification.dish_id = nil
+    expect(notification).not_to be_valid
+  end
+
+  it "varietyがnilの場合、無効であること" do
+    notification.variety = nil
+    expect(notification).not_to be_valid
+  end
+
+  it "from_user_idがnilの場合、無効であること" do
+    notification.from_user_id = nil
+    expect(notification).not_to be_valid
+  end
 end
