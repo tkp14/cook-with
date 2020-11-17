dc3085f2280ac768f6e0cf87c282736a16d9903c
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-11-17 15:54:56 +0900

    Userモデルにnotificationカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_17_051739) do
+ActiveRecord::Schema.define(version: 2020_11_17_065023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,6 +81,7 @@ ActiveRecord::Schema.define(version: 2020_11_17_051739) do
     t.string "password_digest"
     t.string "remember_digest"
     t.boolean "admin", default: false
+    t.boolean "notification", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
