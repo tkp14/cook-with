10a07f661a5732894b82df1dc4b50a805da0e583
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-11-17 16:01:47 +0900

    Favoritesコントローラに通知を生成する処理を追加

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -13,6 +13,12 @@ class FavoritesController < ApplicationController
       format.html { redirect_to request.referrer || root_url }
       format.js
     end
+    # 自分以外のユーザーからお気に入り登録があったときのみ通知を作成
+    if @user != current_user
+      @user.notifications.create(dish_id: @dish.id, variety: 1,
+                                 from_user_id: current_user.id)# お気に入り登録は通知種別1
+      @user.update_attribute(:notification, true)
+    end
   end
 
   def destroy
