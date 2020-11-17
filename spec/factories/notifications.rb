a4c557a8501c6d8949c9e307e53a5eb5acf5e199
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-11-17 14:40:33 +0900

    関連付け,バリデーション,降順の設定を追加

--- spec/factories/notifications.rb
+++ spec/factories/notifications.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :notification do
-    user_id 1
-    dish_id 1
-    variety 1
-    content "MyText"
-    from_user_id 1
+    dish_id { 1 }
+    variety { 1 }
+    content { "" }
+    from_user_id { 2 }
+    association :user
   end
 end
