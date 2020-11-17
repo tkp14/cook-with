9c771f87675e507086d56e70875a66e2c7adb646
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-11-17 14:54:26 +0900

    コントローラー,ルーティング作成

---
+++ app/controllers/notifications_controller.rb
@@ -0,0 +1,5 @@
+class NotificationsController < ApplicationController
+  before_action :logged_in_user
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -19,4 +19,5 @@ Rails.application.routes.draw do
   post   "favorites/:dish_id/create"  => "favorites#create"
   delete "favorites/:dish_id/destroy" => "favorites#destroy"
   resources :comments, only: [:create, :destroy]
+  resources :notifications, only: :index
 end
