aae4275d603495d2a66eb757acb8e37ce2565bd0
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-11-18 19:49:52 +0900

    テスト作成

--- spec/models/list_spec.rb
+++ spec/models/list_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe List, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:lists) { create(:lists) }
+
+  it "リストが有効であること" do
+    expect(list).to be_valid
+  end
+
+  it "user_idがnilの場合、無効であること" do
+    list.user_id = nil
+    expect(list).not_to be_valid
+  end
+
+  it "dish_idがnilの場合、無効であること" do
+    list.dish_id = nil
+    expect(list).not_to be_valid
+  end
+
+  it "from_user_idがnilの場合、無効であること" do
+    list.from_user_id = nil
+    expect(list).not_to be_valid
+  end
 end
