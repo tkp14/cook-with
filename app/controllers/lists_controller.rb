e7d39406c98f862853d35c31c60d65c5b8b49c35
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-11-18 22:23:16 +0900

    リスト登録,削除機能を実装

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,12 +1,26 @@
 class ListsController < ApplicationController
   before_action :logged_in_user
-  
+
   def index
   end
 
   def create
+    @dish = Dish.find(params[:dish_id])
+    @user = @dish.user
+    current_user.list(@dish)
+    respond_to do |format|
+      format.html { redirect_to request.referrer || root_url }
+      format.js
+    end
   end
 
   def destroy
+    list = List.find(params[:list_id])
+    @dish = list.dish
+    list.destroy
+    respond_to do |format|
+      format.html { redirect_to request.referrer || root_url }
+      format.js
+    end
   end
 end
