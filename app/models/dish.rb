73346cd848ddebc69a69b3cc547d5db4b4ad0b11
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-11-20 00:25:04 +0900

    モデルに関連付けとバリデーションを追加

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -3,6 +3,7 @@ class Dish < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :lists, dependent: :destroy
+  has_many :logs, dependent: :destroy
   default_scope -> { order(created_at: :desc) }
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
--- app/models/log.rb
+++ app/models/log.rb
@@ -1,2 +1,5 @@
 class Log < ApplicationRecord
+  belongs_to :dish
+  default_scope -> { order(created_at: :desc) }
+  validates :dish_id, presence: true
 end
