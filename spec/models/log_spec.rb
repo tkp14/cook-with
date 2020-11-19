9ad8ad34d724c75eb3872de2f4cb54e5e44e027b
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-11-20 00:34:19 +0900

    テスト作成

--- spec/models/log_spec.rb
+++ spec/models/log_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Log, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:log) { create(:log) }
+
+  it "ログが有効であること" do
+    expect(log).to be_valid
+  end
+
+  it "dish_idがnilの場合、無効であること" do
+    log = build(:log, dish_id: nil)
+    expect(log).not_to be_valid
+  end
 end
