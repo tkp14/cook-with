04f3b64750afa6d2c32cd072cfc9d65a2e849082
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-11-24 16:59:20 +0900

    各CSS,JSファイルをprecompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,9 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+Rails.application.config.assets.precompile += %w(
+  animate.css icomoon.css bootstrap.css flexslider.css \
+  style.css magnific-popup.css modernizr-2.6.2.min.js \
+  jquery.min.js jquery.easing.1.3.js bootstrap.min.js \
+  jquery.waypoints.min.js main.js jquery.flexslider-min.js custom.js)
