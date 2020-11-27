0aabbec7d6a3866840ddfe3e44d2f6c3c1917790
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-11-28 00:41:46 +0900

    applicationコントローラーにset_searchメソッドを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :set_search
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  # フィードから検索条件に該当する料理を検索
+  def set_search
+    if logged_in?
+      @search_word = params[:q][:name_cont] if params[:q]
+      @q = current_user.feed.paginate(page: params[:page], per_page: 5).ransack(params[:q])
+      @dishes = @q.result(distinct: true)
+    end
+  end
 
   private
     # ログイン済みユーザーかどうか確認
