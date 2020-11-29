124175068073f0b1ea566ad3bca731d3b2dd5824
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-11-29 13:21:51 +0900

    モデルの関連付け

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -4,6 +4,8 @@ class Dish < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :lists, dependent: :destroy
   has_many :logs, dependent: :destroy
+  has_many :ingredients, dependent: :destroy
+  accepts_nested_attributes_for :ingredients
   default_scope -> { order(created_at: :desc) }
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
