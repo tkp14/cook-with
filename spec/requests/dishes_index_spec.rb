ce4fa7e841f7e85115f67446f2e9f94e54c14d5f
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-12-01 02:29:00 +0900

    テスト作成

--- spec/requests/dishes_index_spec.rb
+++ spec/requests/dishes_index_spec.rb
@@ -11,6 +11,12 @@ RSpec.describe "料理一覧ページ", type: :request do
       expect(response).to have_http_status "200"
       expect(response).to render_template('dishes/index')
     end
+
+    it "CSV出力がエラーなく行えること" do
+      login_for_request(user)
+      get dishes_path(format: :csv)
+      expect(response).to have_http_status "200"
+    end
   end
 
   context "ログインしていない場合" do
--- spec/system/dishes_spec.rb
+++ spec/system/dishes_spec.rb
@@ -373,4 +373,26 @@ RSpec.describe "Dishes", type: :system do
       end
     end
   end
+
+  describe "料理一覧ページ" do
+    context "CSV出力機能" do
+      before do
+        login_for_system(user)
+      end
+
+      it "トップページからCSV出力が行えること" do
+        visit root_path
+        click_link 'みんなの料理をCSV出力'
+        expect(page.response_headers['Content-Disposition']).to \
+          include("みんなの料理一覧_#{Time.current.strftime('%Y%m%d_%H%M')}.csv")
+      end
+
+      it "プロフィールページからCSV出力が行えること" do
+        visit user_path(user)
+        click_link 'みんなの料理をCSV出力'
+        expect(page.response_headers['Content-Disposition']).to \
+          include("みんなの料理一覧_#{Time.current.strftime('%Y%m%d_%H%M')}.csv")
+      end
+    end
+  end
 end
