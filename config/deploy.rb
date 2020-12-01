977d5d9ebdc1e801ea033b8424aee1b81356b539
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-12-01 23:47:29 +0900

    deploy.rbの編集

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,78 +5,27 @@ set :application, "cook-with"
 
 set :repo_url, "git@github.com:tkp14/cook-with.git"
 
-# ソケットの場所、Nginxとのやり取りに必要
-set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"
-# サーバー状態を表すファイルの場所
-set :puma_state, "#{shared_path}/tmp/pids/puma.state"
-# プロセスを表すファイルの場所
-set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
-# ログの場所
-set :puma_access_log, "#{shared_path}/log/puma.error.log"
-set :puma_error_log, "#{shared_path}/log/puma.access.log"
-# Rubyをrbenv経由で使う
+# バージョンが変わっても共通で参照するディレクトリを指定
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.7'
-set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-# タスクでsudoなどを行う際に必要
-set :pty, true
-# シンボリックリンクのファイルを指定、具体的にはsharedに入るファイル
-set :linked_files, %w[config/master.key]
-# シンボリックリンクのディレクトリを生成
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "public/uploads", "public/storage"
-# 保存しておく過去分のアプリ数
-set :keep_releases, 3
 
+# どの公開鍵を利用してデプロイするか
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['~/.ssh/taka_cook-with.pem']
+
+# プロセス番号を記載したファイルの場所
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+# Unicornの設定ファイルの場所
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+# デプロイ処理が終わった後、Unicornを再起動するための記述
+after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
-  desc 'Create database'
-  task :db_create do
-    on roles(:db) do
-      with rails_env: fetch(:rails_env) do
-        within current_path do
-          execute :bundle, :exec, :rails, 'db:create'
-        end
-      end
-    end
-  end
-
-  desc 'Restart application'
   task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      invoke 'puma:restart'
-    end
+    invoke 'unicorn:restart'
   end
 end
-
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
-
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
-
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
