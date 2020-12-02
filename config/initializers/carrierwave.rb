35f17cf5cdf31ffb14a3ef386a0d3fb058e4b47b
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-12-02 23:06:37 +0900

    config.yml修正5

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,17 +1,13 @@
-require 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
-
-CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    :provider              => 'AWS',
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      # Amazon S3用の設定
+      :provider              => 'AWS',
       :region                => ENV['S3_REGION'],
       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET_KEY']
-  }
-
-  config.fog_directory  = 'taka-cook-with'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/taka-cook-with'
+    }
+    config.fog_directory     =  ENV['S3_BUCKET']
+  end
 end
