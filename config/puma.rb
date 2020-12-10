fff7ff19abb342d5c28627f398f73820cfd43611
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-12-10 23:38:17 +0900

    capistrano関連修正4

--- config/puma.rb
+++ config/puma.rb
@@ -1,10 +1,15 @@
-threads_count = ENV.fetch('RAILS_MAX_THREADS') { 5 }.to_i
+# アプリケーションディレクトリ
+app_dir = File.expand_path("../../", __FILE__)
+# ソケット通信を図る為bindでURI指定
+bind "unix://#{app_dir}/tmp/sockets/puma.sock"
+# PIDファイル所在(プロセスID)
+pidfile "#{app_dir}/tmp/pids/puma.pid"
+# stateファイルはpumactlコマンドでサーバーを操作する。その所在。
+state_path "#{app_dir}/tmp/pids/puma.state"
+# 標準出力/標準エラーを出力するファイルの所在。
+stdout_redirect "#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.log", true
+threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
-port        ENV.fetch('PORT') { 3000 }
-environment ENV.fetch('RAILS_ENV') { 'development' }
+#port       ENV.fetch("PORT") { 3000 }
+environment ENV.fetch("RAILS_ENV") { "development" }
 plugin :tmp_restart
-
-app_root = File.expand_path('..', __dir__)
-bind "unix://#{app_root}/tmp/sockets/puma.sock"
-
-stdout_redirect "#{app_root}/log/puma.stdout.log", "#{app_root}/log/puma.stderr.log", true
