833281541c6eb1927b3ab0076cabe709fd5f9f92
外山貴啓 <toyamatakahiro@toyamakikeinoMacBook-Air.local>
2020-12-12 18:37:51 +0900

    capistrano関連修正14

--- config/puma.rb
+++ config/puma.rb
@@ -10,6 +10,6 @@ state_path "#{app_dir}/tmp/pids/puma.state"
 stdout_redirect "#{app_dir}/log/puma.stdout.log", "#{app_dir}/log/puma.stderr.log", true
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
-#port       ENV.fetch("PORT") { 3000 }
+port       ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { "development" }
 plugin :tmp_restart
